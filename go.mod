187fdbae234e377ca326f7cdcd5331f258e6b984
Artsiom Hontar <artsiom.hontar@ihsmarkit.com>
2021-05-12 16:13:41 +0300

    feat: add status checks for pod

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module example.com/hello
 go 1.15
 
 require (
+	k8s.io/api v0.19.2
 	k8s.io/apimachinery v0.21.0
 	k8s.io/client-go v0.19.2
 )
--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-    "context"
+	"context"
 	"flag"
 	"fmt"
 	"path/filepath"
-	"time"
-    "k8s.io/apimachinery/pkg/api/errors"
+
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
@@ -14,7 +14,7 @@ import (
 )
 
 func main() {
-    var kubeconfig *string
+	var kubeconfig *string
 	if home := homedir.HomeDir(); home != "" {
 		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
 	} else {
@@ -22,7 +22,7 @@ func main() {
 	}
 	flag.Parse()
 
-    // use the current context in kubeconfig
+	// use the current context in kubeconfig
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
 		panic(err.Error())
@@ -33,30 +33,45 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-    for {
-		pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
-		if err != nil {
-			panic(err.Error())
-		}
-		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
-
-		// Examples for error handling:
-		// - Use helper functions like e.g. errors.IsNotFound()
-		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-		namespace := "default"
-		pod := "example-xxxxx"
-		_, err = clientset.CoreV1().Pods(namespace).Get(context.TODO(), pod, metav1.GetOptions{})
-		if errors.IsNotFound(err) {
-			fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
-		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-			fmt.Printf("Error getting pod %s in namespace %s: %v\n",
-				pod, namespace, statusError.ErrStatus.Message)
-		} else if err != nil {
-			panic(err.Error())
-		} else {
-			fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
-		}
-
-		time.Sleep(10 * time.Second)
+	pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
 	}
+	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+	podRunningCount, podSucceededCount := 0, 0
+	var unhealthyPods [100]v1.Pod
+	for i := 0; i < len(pods.Items); i++ {
+		pod := pods.Items[i]
+		if pod.Status.Phase == v1.PodRunning { // Step 1: If any pods in running status
+			podRunningCount++
+			fmt.Printf("Pod %s is Running\n", pod.Name)
+		} else if pod.Status.Phase == v1.PodSucceeded { // Step 1: If any pods in succeeded status
+			podSucceededCount++
+			fmt.Printf("Pod %s is Succeeded\n", pod.Name)
+		} else {
+			unhealthyPods[i] = pod
+		}
+	}
+
+	fmt.Printf("Pods in running: %d\n", podRunningCount)
+	fmt.Printf("Pods in ready: %d\n", podSucceededCount)
+
+	podPendingCount, podFailedCount, podUnknownCount := 0, 0, 0
+	for i := 0; i < len(unhealthyPods); i++ {
+		unhealthyPod := unhealthyPods[i]
+		if unhealthyPod.Status.Phase == v1.PodPending { // Step 3: If any pods in pending status
+			podPendingCount++
+			fmt.Printf("Pod %s is Pending\n", unhealthyPod.Name)
+		} else if unhealthyPod.Status.Phase == v1.PodFailed { // Step 4: If any pods in failed status
+			podFailedCount++
+			fmt.Printf("Pod %s is Failed\n", unhealthyPod.Name)
+		} else if unhealthyPod.Status.Phase == v1.PodUnknown { // Step 5: If any pods in unknown status
+			podUnknownCount++
+			fmt.Printf("Pod %s is Unknown\n", unhealthyPod.Name)
+		}
+	}
+
+	fmt.Printf("Pods in pending: %d\n", podPendingCount)
+	fmt.Printf("Pods in failed: %d\n", podFailedCount)
+	fmt.Printf("Pods in unknown: %d\n", podUnknownCount)
 }
